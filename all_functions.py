4a25309d8d3aa1b38b79002025b8d1f7a0cd1b80
lokeshdangii <lokeshdangi1045@gmail.com>
2024-01-08 16:53:06 +0530

    removed db_config from all functions.py

--- all_functions.py
+++ all_functions.py
@@ -7,12 +7,6 @@ import random
 # Initialize the Faker instance
 fake = Faker()
 
-# # Define your MySQL database configuration
-# db_config = {
-#     'host': 'localhost',
-#     'user': 'root',
-#     'password': '9644'
-# }
 
 # app = Flask(__name__)
 
